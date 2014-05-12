95dfec5835a990d5e1a08fbee409033027466d42
amroelmo <Amro Elmo>
2014-05-11 23:23:43 -0400

    Adding WorkoutSet Weight/Reps Getters and Setters

--- src/com/sourceforge/workout_timeline/models/WorkoutSet.java
+++ src/com/sourceforge/workout_timeline/models/WorkoutSet.java
@@ -11,12 +11,13 @@ import com.orm.SugarRecord;
  * @author amro
  * 
  */
-public class WorkoutSet extends SugarRecord<WorkoutSet> implements Comparable<WorkoutSet>{
+public class WorkoutSet extends SugarRecord<WorkoutSet> implements
+		Comparable<WorkoutSet> {
 
 	public String muscle;
 	public String exercise;
-	public String weight;
-	public String reps;
+	public Integer weight;
+	public Integer reps;
 	public Timestamp posted_date;
 	public Context ctx;
 
@@ -33,7 +34,8 @@ public class WorkoutSet extends SugarRecord<WorkoutSet> implements Comparable<Wo
 		return Exercise.valueOf(exercise);
 	}
 
-	public static WorkoutSet clone(WorkoutSet original) throws CloneNotSupportedException {
+	public static WorkoutSet clone(WorkoutSet original)
+			throws CloneNotSupportedException {
 		return (WorkoutSet) original.clone();
 	}
 
@@ -45,8 +47,8 @@ public class WorkoutSet extends SugarRecord<WorkoutSet> implements Comparable<Wo
 		copy.exercise = exercise;
 		copy.weight = weight;
 		copy.reps = reps;
-//		copy.posted_date = (Date) posted_date.clone();
-		copy.posted_date =new Timestamp( new Date().getTime());
+		// copy.posted_date = (Date) posted_date.clone();
+		copy.posted_date = new Timestamp(new Date().getTime());
 		return copy;
 	}
 
@@ -54,14 +56,70 @@ public class WorkoutSet extends SugarRecord<WorkoutSet> implements Comparable<Wo
 	public int compareTo(WorkoutSet another) {
 		return -1 * getId().compareTo(another.getId());
 	}
-	
-	public static WorkoutSet createWorkoutSet(Context ctx){
+
+	public static WorkoutSet createWorkoutSet(Context ctx) {
 		WorkoutSet w = new WorkoutSet(ctx);
 		w.muscle = Muscle.EMPTY.name();
 		w.exercise = Exercise.EMPTY.name();
 		w.posted_date = new Timestamp(new Date().getTime());
 		return w;
 	}
-	
-	
+
+	public Integer setWeight(int hunders, int tens, int units) {
+		int result = 0;
+		if (hunders != 0) {
+			result += 100 * hunders;
+		}
+		if (tens != 0) {
+			result += 10 * tens;
+		}
+		if (units != 0) {
+			result += units;
+		}
+		return result;
+	}
+
+	public int[] getWeight() {
+		int[] result = new int[3];
+		int number = weight.intValue();
+		result[0] = number % 100;
+		if (result[0] != 0) {
+			number -= 100;
+		}
+		result[1] = number % 10;
+		if (result[1] != 0) {
+			number -= 10;
+		}
+		result[2] = number % 1;
+		if (result[1] != 0) {
+			number -= 1;
+		}
+		return result;
+	}
+
+	public int[] getReps() {
+		int[] result = new int[2];
+		int number = weight.intValue();
+		result[0] = number % 10;
+		if (result[1] != 0) {
+			number -= 10;
+		}
+		result[1] = number % 1;
+		if (result[1] != 0) {
+			number -= 1;
+		}
+		return result;
+	}
+
+	public Integer setReps(int tens, int units) {
+		int result = 0;
+		if (tens != 0) {
+			result += 10 * tens;
+		}
+		if (units != 0) {
+			result += units;
+		}
+		return result;
+	}
+
 }
---
+++ test/com/sourceforge/workout_timeline/test/TestWorkoutSetWeightGetterSetter.java
@@ -0,0 +1,15 @@
+package com.sourceforge.workout_timeline.test;
+
+import junit.framework.TestCase;
+
+import com.sourceforge.workout_timeline.models.WorkoutSet;
+
+//import com.sourceforge.workout_timeline.models.WorkoutSet;
+
+public class TestWorkoutSetWeightGetterSetter extends TestCase {
+	WorkoutSet set = new WorkoutSet(null);
+	WorkoutSet set1 = set;
+
+	
+
+}
